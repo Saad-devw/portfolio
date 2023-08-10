a8b48a516153ef4806010aa8ee03fbf6619ab5ac
saad essadiki <saadessadiki2017@gmail.com>
2023-08-10 02:59:51 +0100

    local changes

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,14 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+let loader = document.getElementById('loader');
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  setTimeout(() => {
+    loader?.classList.add('fade-loader');
+  }, 500);
+  setTimeout(() => {
+    loader?.classList.add('hidden');
+  }, 1000);
+})
